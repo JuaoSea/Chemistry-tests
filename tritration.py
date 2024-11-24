d389a6b02b1fd6c3d4874721a18a6c7e76795ad7
João Vitor <jvitorm14@gmail.com>
2024-11-24 19:31:30 -0300

    1.0

--- tritration.py
+++ tritration.py
@@ -11,10 +11,10 @@ desvio_padrao = np.std(volumes, ddof=1)  # Desvio padrão
 desvio_relativo = (desvio_padrao / media) * 100  # Desvio relativo
 
 # Mol de AgNO3 (igual a mol de NaCl, pela estequiometria)
-moles_NaCl = (media / 1000) * conc_AgNO3  # Convertendo média de mL para L
+mol_NaCl = (media / 1000) * conc_AgNO3  # Convertendo média de mL para L
 
 # Massa de NaCl em g
-massa_NaCl = moles_NaCl * mm_NaCl
+massa_NaCl = mol_NaCl * mm_NaCl
 
 # % m/v de NaCl (massa em g por 100 mL)
 conc_NaCl_mv = (massa_NaCl / (media / 1000)) * 100  # Considerando volume titulado médio
